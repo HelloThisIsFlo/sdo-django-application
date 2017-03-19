800013c1a475908782324f4bb51f1906f1691008
Florian Kempenich <shockn745@gmail.com>
2017-03-19 11:46:28 +0000

    Add TOX to the project

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+
+    def test_first_test(self):
+        print("Hello :D This is a test")
+        self.assertIs(1 == 2, False)
