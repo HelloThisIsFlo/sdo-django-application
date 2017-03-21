5019291ebcb8898d878141139e3c4ebeae055958
Florian Kempenich <shockn745@gmail.com>
2017-03-21 08:02:40 +0000

    add test hitting database

--- polls/tests.py
+++ polls/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from .models import Question
+from django.utils import timezone
 
 # Create your tests here.
 class QuestionMethodTests(TestCase):
@@ -6,3 +8,10 @@ class QuestionMethodTests(TestCase):
     def test_first_test(self):
         print("Hello :D This is a test")
         self.assertIs(1 == 2, False)
+
+    def test_with_db(self):
+        q = Question(question_text="Test question question question question question :D ", pub_date=timezone.now())
+        q.save()
+
+        all = Question.objects.all()
+        self.assertEqual(len(all), 1)
