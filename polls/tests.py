a95905835baddfcf0ee37ae1806a537eedd7e5ec
root <root@florian-sandbox.localdomain>
2017-03-25 19:03:57 +0000

    Testing github webhooks

--- polls/tests.py
+++ polls/tests.py
@@ -17,7 +17,7 @@ class QuestionMethodTests(TestCase):
         self.assertEqual(len(all), 1)
 
     def test_another_test(self):
-        print("Webhooks ????")
+        print("Testing github webhooks")
         self.assertEqual(1, 1)
 
     def test_one_more_test(self):
