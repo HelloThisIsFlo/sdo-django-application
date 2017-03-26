fa7b7f8cb65bc2e2d7f9fa3ed24d8d5f20fbd0a5
Florian Kempenich <Shockn745@gmail.com>
2017-03-26 13:34:52 +0000

    test failing pipeline

--- polls/tests.py
+++ polls/tests.py
@@ -21,6 +21,6 @@ class QuestionMethodTests(TestCase):
         self.assertEqual(1, 1)
 
     def test_one_more_test(self):
-        print("One More test")
-        self.assertEqual(1, 1)
+        print("Test Failing pipeline")
+        self.assertEqual(1, 2)
 
